c9a556028593b9717ff83930103af0d91089f7ba
Philip Smith <pmsmith@gmail.com>
2020-07-08 11:29:52 -0700

    Updated scratch.py

--- scratch.py
+++ scratch.py
@@ -1,4 +1,13 @@
-import xml.etree.ElementTree as try:
-    pass
-except expression as identifier:
-    pass
+import xml.etree.ElementTree as ET
+
+stream = open('sample.xml','r')
+
+xml = ET.parse(stream)
+
+root = xml.getroot()
+
+for e in root:
+    print(ET.tostring(e))
+    print("")
+
+    print(e.get("Id"))
